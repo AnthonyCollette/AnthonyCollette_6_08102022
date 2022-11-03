587596750d55f4b5f875f53050a9403ea9318466
Anthony Collette <100pour100inde@gmail.com>
2022-11-04 00:29:02 +0100

    btn style

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,17 +1,15 @@
 const btn = document.getElementById('contact')
 const closeModalBtn = document.getElementById('close-modal-btn')
-const modal = document.getElementById("contact_modal");
-
-
+const modal = document.getElementById("contact_modal")
 
 // Ouverture du modal
 btn.addEventListener('click', () => {
-	modal.classList.add("display-flex");
+    modal.classList.add("display-flex")
 })
 
 
 // Fermeture du modal
 closeModalBtn.addEventListener('click', () => {
-    modal.classList.remove("display-flex");
+    modal.classList.remove("display-flex")
 })
 
